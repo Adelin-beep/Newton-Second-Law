bb464864e1d4b5ee966753c3f4fb28a3e8ba1bd1
Adelin-beep <73435099+Adelin-beep@users.noreply.github.com>
2021-01-06 23:41:41 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+/** ===Algoritma===
+1. Masukkan nilai massa
+2. Masukkan nilai percepatan
+3. Siapkan variabel resultanGaya untuk menampung hasil perhitungan
+4. Hitung nilai resultanGaya dengan cara mengalikan poin 1 dan 2
+5. Tampilkan nilai resultanGaya
+*/
+
+//===Pseudocode===
+// STORE massa WITH 600
+// STORE percepatan WITH 2
+// STORE resultanGaya WITH ANY NUMBER 
+
+// SET resultanGaya WITH massa TIMES percepatan
+// DISPLAY "resultanGaya = " CONCAT WITH resultanGaya
+
+// Insert your code here
+var massa;
+var massa = 600;
+var percepatan;
+var percepatan = 2;
+var resultanGaya;
+resultanGaya = massa * percepatan;
+console.log(resultanGaya);
